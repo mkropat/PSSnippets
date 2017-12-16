3f3ea81e9757d3f49a52143954b7e8e25802fb1a
Michael Kropat <mail@michael.kropat.name>
2017-12-16 12:05:01 -0500

    Create Set-GitConfig.ps1

---
+++ Setup/Dev/Set-GitConfig.ps1
@@ -0,0 +1,12 @@
+git config --global core.commentchar ";"
+git config --global push.default simple
+git config --global core.autocrlf input
+
+git config --global alias.ci 'commit --verbose'
+git config --global alias.co checkout
+git config --global alias.dc 'diff --cached'
+git config --global alias.di diff
+git config --global alias.ff 'merge --ff-only'
+git config --global alias.noff 'merge --no-ff'
+git config --global alias.pullff 'pull --ff-only'
+git config --global alias.st 'status --short'

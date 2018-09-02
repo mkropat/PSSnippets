dfc25fcf06c74bebe31559e7fd07525768b14854
Michael Kropat <mail@michael.kropat.name>
2018-09-02 12:23:45 -0400

    Create Install-AhkScript.ps1

---
+++ Setup/Win10/Install-AhkScript.ps1
@@ -0,0 +1,33 @@
+#Requires -RunAsAdministrator
+
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    $Script,
+)
+
+$ErrorActionPreference = 'Stop'
+
+$ahkAssociation = Get-ItemProperty Registry::HKEY_CLASSES_ROOT\.ahk -ErrorAction SilentlyContinue | Select-Object -ExpandProperty '(default)'
+if ($ahkAssociation -ne 'AutoHotkeyScript') {
+    if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
+        if (@('AllSigned', 'Default', 'Restricted', 'Undefined') -contains (Get-ExecutionPolicy)) {
+            Set-ExecutionPolicy Bypass -Scope Process -Force
+        }
+
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    }
+    
+    choco install -y autohotkey
+}
+
+$scriptItem = Get-Item $Script
+
+$lnkPath = Join-Path ([Environment]::GetFolderPath('Startup')) "$($scriptItem.BaseName).lnk"
+
+$wsh = New-Object -ComObject WScript.Shell
+$shortcut = $wsh.CreateShortcut($lnkPath)
+$shortcut.TargetPath = $scriptItem.FullName
+$shortcut.Save()
+
+& $lnkPath

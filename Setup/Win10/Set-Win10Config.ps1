f8311c1be3a714fb357b9c2153615eee4f990324
Michael Kropat <mail@michael.kropat.name>
2018-09-03 10:38:41 -0400

    Update Set-Win10Config.ps1

--- Setup/Win10/Set-Win10Config.ps1
+++ Setup/Win10/Set-Win10Config.ps1
@@ -17,7 +17,7 @@ if ($PresetUrl) {
 }
 
 $scriptUrl = 'https://raw.githubusercontent.com/Disassembler0/Win10-Initial-Setup-Script/master/Win10.ps1'
-(New-Object Net.WebClient).DownloadString($url) | Out-File "$env:TEMP\$(Split-Path -Leaf $scriptUrl)"
+(New-Object Net.WebClient).DownloadString($scriptUrl) | Out-File "$env:TEMP\$(Split-Path -Leaf $scriptUrl)"
 
 if ($PresetUrl) {
     & "$env:TEMP\$(Split-Path -Leaf $scriptUrl)" -Preset "$TEMP\$(Split-Path -Leaf $PresetUrl)
